1fb0692cb01e0f85a9d0e3dd8e3d434b4ee767b6
MoonFruit <dkmoonfruit@gmail.com>
2018-08-22 21:11:48 +0800

    Use centos 7 to compile lib

--- cgo.go
+++ cgo.go
@@ -3,7 +3,7 @@ package bls
 /*
 #cgo CFLAGS:-I${SRCDIR}/include -DMCLBN_FP_UNIT_SIZE=6
 #cgo LDFLAGS:-lbls384 -lmcl -lcrypto -lgmp -lgmpxx -lstdc++
-#cgo linux,amd64 LDFLAGS:-L${SRCDIR}/lib/linux/amd64
+#cgo linux,amd64 LDFLAGS:-L${SRCDIR}/lib/linux/amd64 -ldl -lz
 #cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/lib/darwin/amd64
 #cgo windows,amd64 LDFLAGS:-L${SRCDIR}/lib/windows/amd64 -static
 #include <bls/bls.h>
