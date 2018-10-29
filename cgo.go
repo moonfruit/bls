9f5d91c1ab92bbf973c4afef6f87da76656f57f8
MoonFruit <dkmoonfruit@gmail.com>
2018-10-29 15:12:22 +0800

    Delete unused lib libgmpxx

--- cgo.go
+++ cgo.go
@@ -2,7 +2,7 @@ package bls
 
 /*
 #cgo CFLAGS:-I${SRCDIR}/include -DMCLBN_FP_UNIT_SIZE=6
-#cgo LDFLAGS:-lbls384 -lmcl -lcrypto -lgmp -lgmpxx -lstdc++
+#cgo LDFLAGS:-lbls384 -lmcl -lcrypto -lgmp -lstdc++
 #cgo linux,amd64 LDFLAGS:-L${SRCDIR}/lib/linux/amd64
 #cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/lib/darwin/amd64
 #cgo windows,amd64 LDFLAGS:-L${SRCDIR}/lib/windows/amd64 -static
