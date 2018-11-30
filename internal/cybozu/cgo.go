bc5edd78dbe76a1b3feffedd4085abbf22d365f8
MoonFruit <dkmoonfruit@gmail.com>
2018-11-30 09:45:42 +0800

    use sync.Map for sha

---
+++ internal/cybozu/cgo.go
@@ -0,0 +1,6 @@
+package sha
+
+/*
+#cgo CPPFLAGS: -DMCL_DONT_USE_OPENSSL
+*/
+import "C"
---
+++ internal/openssl/cgo.go
@@ -0,0 +1,7 @@
+package sha
+
+/*
+#cgo LDFLAGS: -lcrypto
+#cgo darwin LDFLAGS: -L/usr/local/opt/openssl/lib
+ */
+import "C"
